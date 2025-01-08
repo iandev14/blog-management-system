efd2b7037bca78ee180bd45bf7b39851614cc26a
Nicolas <mark20nicolas@gmail.com>
2025-01-08 17:49:29 +0800

    Add login controller, components, update routes, modify views and layout

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,3 +1,34 @@
-<div>
-    <!-- I have not failed. I've just found 10,000 ways that won't work. - Thomas Edison -->
-</div>
+<nav class="flex items-center px-28 py-5 bg-gray-800 text-white sticky top-0 z-50">
+    <!-- Logo -->
+    <img src="{{ asset('assets/images/logo.png') }}" class="w-10 cursor-pointer">
+    
+    <!-- Navigation Links -->
+    <ul class="flex-1 text-center">
+        <li class="list-none inline-block px-5">
+            <a href="{{ route('welcome') }}" class="no-underline text-white">Home</a>
+        </li>
+        <li class="list-none inline-block px-5">
+            <a href="#" class="no-underline text-white">Features</a>
+        </li>
+        <li class="list-none inline-block px-5">
+            <a href="#" class="no-underline text-white">About Us</a>
+        </li>
+        <li class="list-none inline-block px-5">
+            <a href="#" class="no-underline text-white">Help</a>
+        </li>
+        <li class="list-none inline-block px-5">
+            <a href="#" class="no-underline text-white">Blog</a>
+        </li>
+        <li class="list-none inline-block px-5">
+            <a href="#" class="no-underline text-white">Contact</a>
+        </li>
+        <li class="list-none inline-block px-5">
+            <a href="{{ route('login') }}" class="no-underline text-white">Login</a>
+        </li>
+    </ul>
+
+    <!-- Sign Up Button -->
+    <button type="submit" class="bg-gray-800 text-white px-4 py-2 rounded border-2 border-white hover:bg-gray-700">
+        Sign Up
+    </button>
+</nav>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('welcome');
+
+Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
