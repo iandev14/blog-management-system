965cef13d19f52c0f20b0c442730d47f97c50c45
ian <villanuevachristiandel@gmail.com>
2025-01-08 15:39:45 +0800

    update tailwind and remove cdn

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,4 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
-
-/** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
@@ -12,9 +9,15 @@ export default {
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ['Figtree', ...require('tailwindcss/defaultTheme').fontFamily.sans],
+            },
+            backgroundImage: {
+                'hero-pattern': "url('/assets/images/background.jpg')",
             },
         },
     },
     plugins: [],
+    safelist: [
+        'bg-hero-pattern', // Safelist this class to ensure it is included in the CSS
+    ],
 };
